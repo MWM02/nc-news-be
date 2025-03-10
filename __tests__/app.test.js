3e1ad87cc9c2e063f8646066d1a496b508ff2b68
MWM02 <wijem004@gmail.com>
2025-03-10 15:39:50 +0000

    adds new tests for get-articles-by-id endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -30,6 +30,7 @@ describe("GET /api/topics", () => {
       .get("/api/topics")
       .expect(200)
       .then(({ body: { topics } }) => {
+        expect(topics.length).toBe(3);
         topics.forEach((topic) => {
           expect(topic).toMatchObject({
             slug: expect.any(String),
@@ -38,14 +39,54 @@ describe("GET /api/topics", () => {
         });
       });
   });
+});
 
+describe("GET /api/articles/:article_id", () => {
+  test("200: Responds with an article object that will contain the properties: author, title, article_id, body, topic, created_at, votes, article_img_url", () => {
+    return request(app)
+      .get("/api/articles/2")
+      .expect(200)
+      .then(({ body: { article } }) => {
+        expect(article).toEqual({
+          article_id: 2,
+          title: "Sony Vaio; or, The Laptop",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: "Call me Mitchell. Some years ago..",
+          created_at: "2020-10-16T05:03:00.000Z",
+          votes: 0,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+
+  test("400: Responds with an object containing an error message and error code when the requested article_id is of an invalid data type", () => {
+    return request(app)
+      .get("/api/articles/two")
+      .expect(400)
+      .then(({ body: { error } }) => {
+        expect(error.message).toBe("Invalid text representation");
+      });
+  });
+
+  test("404: Responds with an object containing an error message and error code when the requested article_id of valid data type does not exist", () => {
+    return request(app)
+      .get("/api/articles/99999")
+      .expect(404)
+      .then(({ body: { error } }) => {
+        expect(error.message).toBe("No matches found");
+      });
+  });
+});
+
+describe("Other errors", () => {
   test("404: Responds with an object containing an error message and error code when the requested endpoint doesn't match to any api endpoint", () => {
     return request(app)
       .get("/api/top1cs")
       .expect(404)
       .then(({ body: { error } }) => {
         expect(error.message).toBe("Resource not found");
-        expect(error.code).toBe(404);
       });
   });
 });
