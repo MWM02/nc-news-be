0325f902cb0fa07a87c42ce5ac8a304d2b1c7f95
MWM02 <wijem004@gmail.com>
2025-03-10 17:48:17 +0000

    adds fetchArticles to articles.models

--- models/articles.models.js
+++ models/articles.models.js
@@ -12,3 +12,10 @@ exports.fetchArticleById = async (article_id) => {
   }
   return rows[0];
 };
+
+exports.fetchArticles = async () => {
+  const { rows } = await db.query(
+    `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(articles.article_id) ::INT as comment_count  FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY created_at DESC`
+  );
+  return rows;
+};
