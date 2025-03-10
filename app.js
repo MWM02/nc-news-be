45e206809a366e048b1392f7f3a71f5ab38e50a7
MWM02 <wijem004@gmail.com>
2025-03-10 13:47:34 +0000

    adds new endpoint for api/topics and two error handlers

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const { getEndpoints } = require("./controllers/endpoints.controllers");
-
-console.log(getEndpoints);
+const { getTopics } = require("./controllers/topics.controllers");
+const {
+  endpointErrorHandler,
+  handleServerErrors,
+} = require("./controllers/errors.controllers");
 
 app.get("/api", getEndpoints);
+
+app.get("/api/topics", getTopics);
+
+app.all("*", endpointErrorHandler);
+
+app.use(handleServerErrors);
 
 module.exports = app;
