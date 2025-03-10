a3e4352cb9eca3317c72a6aad53e3888e76ddc53
MWM02 <wijem004@gmail.com>
2025-03-10 15:41:22 +0000

    adds new endpoint for articles and adds a custom error handler

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const { getEndpoints } = require("./controllers/endpoints.controllers");
 const { getTopics } = require("./controllers/topics.controllers");
+const { getArticleById } = require("./controllers/articles.controllers");
 const {
   endpointErrorHandler,
+  handlePsqlErrors,
+  handleCustomErrors,
   handleServerErrors,
 } = require("./controllers/errors.controllers");
 
@@ -11,7 +14,13 @@ app.get("/api", getEndpoints);
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticleById);
+
 app.all("*", endpointErrorHandler);
+
+app.use(handlePsqlErrors);
+
+app.use(handleCustomErrors);
 
 app.use(handleServerErrors);
 
