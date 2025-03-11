f976d5cc0b837856ef9747ac57890889c6a8ff4c
MWM02 <wijem004@gmail.com>
2025-03-11 11:33:10 +0000

    adds new endpoint

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const {
   getArticles,
 } = require("./controllers/articles.controllers");
 const {
+  getCommentsByArticleId,
+} = require("./controllers/comments.controllers");
+const {
   endpointErrorHandler,
   handlePsqlErrors,
   handleCustomErrors,
@@ -20,6 +23,8 @@ app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
 
 app.get("/api/articles", getArticles);
+
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
 app.all("*", endpointErrorHandler);
 
