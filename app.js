de255ffb4684aedf22cb9c52bc2b3cdd2d327a19
MWM02 <wijem004@gmail.com>
2025-03-12 10:55:37 +0000

    adds delete /api/comments/:comment_id endpoint

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const {
 const {
   getCommentsByArticleId,
   postCommentByArticleId,
+  deleteCommentById,
 } = require("./controllers/comments.controllers");
 const {
   endpointErrorHandler,
@@ -32,6 +33,8 @@ app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 app.post("/api/articles/:article_id/comments", postCommentByArticleId);
 
 app.patch("/api/articles/:article_id", patchArticleById);
+
+app.delete("/api/comments/:comment_id", deleteCommentById);
 
 app.all("*", endpointErrorHandler);
 
