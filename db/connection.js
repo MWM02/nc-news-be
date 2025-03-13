119a96223fcf8e9fa0e19f3812ff24e1b528c1e1
MWM02 <wijem004@gmail.com>
2025-03-13 14:46:15 +0000

    adds new scripts

--- db/connection.js
+++ db/connection.js
@@ -9,11 +9,11 @@ let db = new Pool();
 const config = {};
 
 if (ENV === "production") {
-  config.connectionString = process.env.PGDATABASE_URL;
+  config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
 
-if (!process.env.PGDATABASE && !process.env.PGDATABASE_URL) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not configured");
 } else {
   console.log(`Connected to ${process.env.PGDATABASE}`);
