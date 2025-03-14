c03ec90d91522d747e4da075e0578a2f0910f605
MWM02 <wijem004@gmail.com>
2025-03-14 15:04:12 +0000

    adds updateCommentById

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -2,6 +2,7 @@ const {
   fetchCommentsByArticleId,
   insertCommentByArticleId,
   removeCommentById,
+  updateCommentById,
 } = require("../models/comments.models");
 
 exports.getCommentsByArticleId = async (req, res, next) => {
@@ -26,6 +27,15 @@ exports.deleteCommentById = async (req, res, next) => {
   try {
     await removeCommentById(req);
     res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.patchCommentById = async (req, res, next) => {
+  try {
+    const result = await updateCommentById(req);
+    res.status(200).send({ comment: result });
   } catch (err) {
     next(err);
   }
