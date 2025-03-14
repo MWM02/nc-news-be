b818ed80171255c555c6d906ebac2de2fbd47ec6
MWM02 <wijem004@gmail.com>
2025-03-14 18:17:16 +0000

    minor refactor to align with MVC and renames variables to clearer names

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,14 +1,15 @@
 const { fetchUsers, fetchUserById } = require("../models/users.models");
 
 exports.getUsers = async (req, res) => {
-  const results = await fetchUsers();
-  res.status(200).send({ users: results });
+  const users = await fetchUsers();
+  res.status(200).send({ users });
 };
 
 exports.getUserById = async (req, res, next) => {
+  const { username } = req.params;
   try {
-    const result = await fetchUserById(req);
-    res.status(200).send({ user: result });
+    const userById = await fetchUserById(username);
+    res.status(200).send({ userById });
   } catch (err) {
     next(err);
   }
--- models/comments.models.js
+++ models/comments.models.js
@@ -6,20 +6,18 @@ const {
 } = require("../utils/utils");
 const format = require("pg-format");
 
-exports.fetchCommentsByArticleId = async (req) => {
-  const { article_id } = req.params;
+exports.fetchCommentsByArticleId = async (article_id) => {
   const sqlStr = `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC`;
   const promises = [
     checkIfExist("articles", "article_id", article_id),
     db.query(sqlStr, [article_id]),
   ];
-  const results = await Promise.all(promises);
-  return results[1].rows;
+  const resolvedPromises = await Promise.all(promises);
+  return resolvedPromises[1].rows;
 };
 
-exports.insertCommentByArticleId = async (req) => {
-  const { article_id } = req.params;
-  const { username, body } = req.body;
+exports.insertCommentByArticleId = async (article_id, reqBody) => {
+  const { username, body } = reqBody;
   const commentData = {
     article_id,
     username,
@@ -32,15 +30,14 @@ exports.insertCommentByArticleId = async (req) => {
   );
   const promises = [
     checkIfExist("articles", "article_id", article_id),
-    verifyReqBody(req.body, ["username", "body"]),
+    verifyReqBody(reqBody, ["username", "body"]),
     db.query(sqlStr),
   ];
-  const results = await Promise.all(promises);
-  return results[2].rows[0];
+  const resolvedPromises = await Promise.all(promises);
+  return resolvedPromises[2].rows[0];
 };
 
-exports.removeCommentById = async (req) => {
-  const { comment_id } = req.params;
+exports.removeCommentById = async (comment_id) => {
   const sqlStr = `DELETE FROM comments WHERE comment_id = $1`;
   const promises = [
     checkIfExist("comments", "comment_id", comment_id),
@@ -49,13 +46,12 @@ exports.removeCommentById = async (req) => {
   await Promise.all(promises);
 };
 
-exports.updateCommentById = async (req) => {
-  const { comment_id } = req.params;
-  const { inc_votes } = req.body;
+exports.updateCommentById = async (comment_id, reqBody) => {
+  const { inc_votes } = reqBody;
   const sqlStr = `UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *`;
   const promises = [
     checkIfExist("comments", "comment_id", comment_id),
-    verifyReqBody(req.body, ["inc_votes"]),
+    verifyReqBody(reqBody, ["inc_votes"]),
     db.query(sqlStr, [inc_votes, comment_id]),
   ];
   const results = await Promise.all(promises);
