ddbf2b0cd48e239d325f10f8fcb3c97778eb9e10
MWM02 <wijem004@gmail.com>
2025-03-14 13:55:06 +0000

    adds /:username endpoint for users router

--- api_routers/subrouters/users.router.js
+++ api_routers/subrouters/users.router.js
@@ -1,6 +1,11 @@
-const { getUsers } = require("../../controllers/users.controllers");
+const {
+  getUsers,
+  getUserById,
+} = require("../../controllers/users.controllers");
 const usersRouter = require("express").Router();
 
 usersRouter.route("/").get(getUsers);
+
+usersRouter.route("/:username").get(getUserById);
 
 module.exports = usersRouter;
