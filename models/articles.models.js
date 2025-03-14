9f9d411a14f59e3a7b853ba9983019bc85b6be2f
MWM02 <wijem004@gmail.com>
2025-03-14 18:24:56 +0000

    adds insertArticle, refactors to align with MVC and renames variables for readability

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,20 +1,22 @@
 const format = require("pg-format");
 const db = require("../db/connection");
-const { checkIfExist, verifyReqBody } = require("../utils/utils");
+const {
+  checkIfExist,
+  verifyReqBody,
+  convertTimestampToDate,
+} = require("../utils/utils");
 
-exports.fetchArticleById = async (req) => {
-  const { article_id } = req.params;
+exports.fetchArticleById = async (article_id) => {
   const sqlStr = `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.body, articles.created_at, articles.votes, articles.article_img_url, COUNT(articles.article_id) ::INT as comment_count  FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`;
   const promises = [
     checkIfExist("articles", "article_id", article_id),
     db.query(sqlStr, [article_id]),
   ];
-  const results = await Promise.all(promises);
-  return results[1].rows[0];
+  const resolvedPromises = await Promise.all(promises);
+  return resolvedPromises[1].rows[0];
 };
 
-exports.fetchArticles = async (req) => {
-  const { sort_by = "created_at", order = "DESC", topic } = req.query;
+exports.fetchArticles = async (sort_by, order, topic) => {
   const values = [];
   const promises = [];
   let sqlPromiseIndex = 0;
@@ -36,20 +38,51 @@ exports.fetchArticles = async (req) => {
     });
   }
   promises.push(db.query(sqlStr, values));
-  const results = await Promise.all(promises);
-  const { rows } = results[sqlPromiseIndex];
+  const resolvedPromises = await Promise.all(promises);
+  const { rows } = resolvedPromises[sqlPromiseIndex];
   return rows;
 };
 
-exports.updateArticleById = async (req) => {
-  const { article_id } = req.params;
-  const { inc_votes } = req.body;
+exports.updateArticleById = async (article_id, reqBody) => {
+  const { inc_votes } = reqBody;
   const sqlStr = `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`;
   const promises = [
     checkIfExist("articles", "article_id", article_id),
-    verifyReqBody(req.body, ["inc_votes"]),
+    verifyReqBody(reqBody, ["inc_votes"]),
     db.query(sqlStr, [inc_votes, article_id]),
   ];
-  const results = await Promise.all(promises);
-  return results[2].rows[0];
+  const resolvedPromises = await Promise.all(promises);
+  return resolvedPromises[2].rows[0];
+};
+
+exports.insertArticle = async (reqBody) => {
+  const {
+    author,
+    title,
+    body,
+    topic,
+    article_img_url = "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+  } = reqBody;
+  const articleData = {
+    author,
+    title,
+    body,
+    topic,
+    article_img_url,
+    created_at: Date.now(),
+  };
+  const sqlStr = format(
+    `INSERT INTO articles (created_at, author, title, body, topic, article_img_url) VALUES %L RETURNING article_id`,
+    [Object.values(convertTimestampToDate(articleData))]
+  );
+  const promises = [
+    verifyReqBody(reqBody, ["author", "title", "body", "topic"]),
+    db.query(sqlStr),
+  ];
+  const resolvedPromises = await Promise.all(promises);
+  const {
+    rows: [{ article_id }],
+  } = resolvedPromises[1];
+  const article = await exports.fetchArticleById(article_id);
+  return article;
 };
