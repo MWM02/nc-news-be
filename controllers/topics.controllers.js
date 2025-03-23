5a9fb51e3a54176e2b2c0b7bc391f31701a75630
MWM02 <wijem004@gmail.com>
2025-03-23 19:30:24 +0000

    adds postTopic

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,6 +1,15 @@
-const { fetchTopics } = require("../models/topics.models");
+const { fetchTopics, insertTopic } = require("../models/topics.models");
 
 exports.getTopics = async (req, res) => {
   const topics = await fetchTopics();
   res.status(200).send({ topics });
+};
+
+exports.postTopic = async (req, res, next) => {
+  try {
+    const topic = await insertTopic(req.body);
+    res.status(201).send({ topic });
+  } catch (err) {
+    next(err);
+  }
 };
