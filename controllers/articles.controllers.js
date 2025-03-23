69298d13becfa09bae916c160d0dd77935f755d6
MWM02 <wijem004@gmail.com>
2025-03-23 21:22:43 +0000

    adds deleteArticleById

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -3,6 +3,7 @@ const {
   fetchArticles,
   updateArticleById,
   insertArticle,
+  removeArticleById,
 } = require("../models/articles.models");
 
 exports.getArticleById = async (req, res, next) => {
@@ -48,6 +49,17 @@ exports.postArticle = async (req, res, next) => {
   try {
     const article = await insertArticle(req.body);
     res.status(201).send({ article });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteArticleById = async (req, res, next) => {
+  const { article_id } = req.params;
+
+  try {
+    await removeArticleById(article_id);
+    res.status(204).send();
   } catch (err) {
     next(err);
   }
