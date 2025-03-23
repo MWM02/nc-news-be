7db154de079472c663587877f8d544016fe07034
MWM02 <wijem004@gmail.com>
2025-03-23 21:30:08 +0000

    adds on delete action for article_id

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -79,7 +79,7 @@ const createComments = async () => {
   await db.query(`
     CREATE TABLE IF NOT EXISTS comments (
     comment_id SERIAL PRIMARY KEY,
-    article_id INT REFERENCES articles(article_id),
+    article_id INT REFERENCES articles(article_id) ON DELETE CASCADE,
     body TEXT NOT NULL,
     votes INT DEFAULT 0,
     author VARCHAR(255) REFERENCES users(username) NOT NULL,
